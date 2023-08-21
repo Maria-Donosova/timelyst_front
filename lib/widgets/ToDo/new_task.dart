a1fdc381bda31969de4707d89f8b42f59d804a6c
Mariia Donosova <mariiadonosova@Mariias-MacBook-Pro.local>
2023-08-21 08:44:20 -0400

    displayed add new task field within add new task widget

--- lib/widgets/ToDo/new_task.dart
+++ lib/widgets/ToDo/new_task.dart
@@ -11,7 +11,7 @@ class NewTaskW extends StatefulWidget {
 class _NewTaskWState extends State<NewTaskW> {
   final titleController = TextEditingController();
   final _form = GlobalKey<FormState>();
-  //final _taskFocusNode = FocusNode();
+  final _taskFocusNode = FocusNode();
   final _taskDescriptionController = TextEditingController();
   //final _taskTypeController = TextEditingController();
   //final _categoryController = TextEditingController();
@@ -44,11 +44,10 @@ class _NewTaskWState extends State<NewTaskW> {
               return GestureDetector(
                   onTap: () {},
                   behavior: HitTestBehavior.opaque,
-                  child: Column(
-                    children: <Widget>[
-                      Stack(
-                        children: <Widget>[
-                          Card(
+                  child: Column(children: <Widget>[
+                    Stack(
+                      children: <Widget>[
+                        Card(
                             elevation: 5,
                             child: Container(
                               padding: EdgeInsets.only(
@@ -69,95 +68,99 @@ class _NewTaskWState extends State<NewTaskW> {
                                 ),
                                 shape: BoxShape.rectangle,
                               ),
-                              child:
-                                  // Mutation(
-                                  //   options: MutationOptions(
-                                  //     document: gql(insertTask()),
-                                  //     fetchPolicy: FetchPolicy.noCache,
-                                  //     onCompleted: (data) {
-                                  //       print(data.toString());
-                                  //       setState(() {
-                                  //         currUserId = (data as Map)['createUser']["id"];
-                                  //         //currUserId = data['createUser']["id"];
-                                  //       });
-                                  //     },
-                                  //   ),
-                                  //   builder: (runMutation, result) {
-                                  //     return Form(
-                                  //       key: _form,
-                                  //       child: Column(
-                                  //         children: <Widget>[
-                                  //           TextFormField(
-                                  //             autocorrect: true,
-                                  //             controller: _taskDescriptionController,
-                                  //             style: Theme.of(context).textTheme.bodyText1,
-                                  //             maxLines: null,
-                                  //             decoration: const InputDecoration(
-                                  //               labelText: 'Add new task',
-                                  //               labelStyle: TextStyle(fontSize: 14),
-                                  //               border: InputBorder.none,
-                                  //               errorStyle: TextStyle(color: Colors.redAccent),
-                                  //             ),
-                                  //             textInputAction: TextInputAction.next,
-                                  //             keyboardType: TextInputType.name,
-                                  //             validator: (value) {
-                                  //               if (value!.isEmpty) {
-                                  //                 return 'Please provide a value.';
-                                  //               }
-                                  //               return null;
-                                  //             },
-                                  //             onFieldSubmitted: (_) {
-                                  //               FocusScope.of(context)
-                                  //                   .requestFocus(_taskFocusNode);
-                                  //             },
-                                  //           ),
-                                  Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
+                              //child:
+                              // Mutation(
+                              //   options: MutationOptions(
+                              //     document: gql(insertTask()),
+                              //     fetchPolicy: FetchPolicy.noCache,
+                              //     onCompleted: (data) {
+                              //       print(data.toString());
+                              //       setState(() {
+                              //         currUserId = (data as Map)['createUser']["id"];
+                              //         //currUserId = data['createUser']["id"];
+                              //       });
+                              //     },
+                              //   ),
+                              //   builder: (runMutation, result) {
+                              //     return Form(
+                              //       key: _form,
+                              child: Column(
                                 children: <Widget>[
-                                  DropdownButton<String>(
-                                    hint: Text(
-                                      'Category',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .titleSmall,
+                                  TextFormField(
+                                    autocorrect: true,
+                                    controller: _taskDescriptionController,
+                                    style:
+                                        Theme.of(context).textTheme.bodyText1,
+                                    maxLines: null,
+                                    decoration: const InputDecoration(
+                                      labelText: 'Add new task',
+                                      labelStyle: TextStyle(fontSize: 14),
+                                      border: InputBorder.none,
+                                      errorStyle:
+                                          TextStyle(color: Colors.redAccent),
                                     ),
-                                    icon: const Icon(Icons.arrow_downward),
-                                    iconSize: 14,
-                                    value: selectedCategory,
-                                    onChanged: (newValue) {
-                                      if (_form.currentState!.validate()) {
-                                        setState(() {
-                                          // selectedCategory = newValue;
-                                          // runMutation({
-                                          //   "task_description":
-                                          //       _taskDescriptionController.text
-                                          //           .trim(),
-                                          //   // "task_type":
-                                          //   //  _taskTypeController.text.trim(),
-                                          //   "category": _selectedCategory,
-                                          //   'userId': currUserId,
-                                          // });
-                                        });
-                                        Navigator.of(context).pop();
-                                        clearInput();
+                                    textInputAction: TextInputAction.next,
+                                    keyboardType: TextInputType.name,
+                                    validator: (value) {
+                                      if (value!.isEmpty) {
+                                        return 'Please provide a value.';
                                       }
+                                      return null;
                                     },
-                                    items: categories.map((category) {
-                                      return DropdownMenuItem(
-                                        child: Text(category),
-                                        value: category,
-                                      );
-                                    }).toList(),
+                                    onFieldSubmitted: (_) {
+                                      FocusScope.of(context)
+                                          .requestFocus(_taskFocusNode);
+                                    },
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: <Widget>[
+                                      DropdownButton<String>(
+                                        hint: Text(
+                                          'Category',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .titleSmall,
+                                        ),
+                                        icon: const Icon(Icons.arrow_downward),
+                                        iconSize: 14,
+                                        value: selectedCategory,
+                                        onChanged: (newValue) {
+                                          if (_form.currentState!.validate()) {
+                                            setState(() {
+                                              // selectedCategory = newValue;
+                                              // runMutation({
+                                              //   "task_description":
+                                              //       _taskDescriptionController.text
+                                              //           .trim(),
+                                              //   // "task_type":
+                                              //   //  _taskTypeController.text.trim(),
+                                              //   "category": _selectedCategory,
+                                              //   'userId': currUserId,
+                                              // });
+                                            });
+                                            Navigator.of(context).pop();
+                                            clearInput();
+                                          }
+                                        },
+                                        items: categories.map((category) {
+                                          return DropdownMenuItem(
+                                            child: Text(category),
+                                            value: category,
+                                          );
+                                        }).toList(),
+                                      ),
+                                    ],
                                   ),
                                 ],
                               ),
+                            )
+                            //],
                             ),
-                          )
-                        ],
-                      ),
-                    ],
-                  ));
+                      ],
+                    ),
+                  ]));
             },
           ),
         ),
