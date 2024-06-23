ea76f34aee151ab9350feac300dbd90484dafe37
Mariia Donosova <mariiadonosova@Mariias-MacBook-Pro.local>
2024-06-22 20:18:17 -0400

    Fix the Edit task

--- lib/widgets/ToDo/new_task.dart
+++ lib/widgets/ToDo/new_task.dart
@@ -13,8 +13,8 @@ class _NewTaskWState extends State<NewTaskW> {
   final _form = GlobalKey<FormState>();
   final _taskFocusNode = FocusNode();
   final _taskDescriptionController = TextEditingController();
-  //final _taskTypeController = TextEditingController();
-  //final _categoryController = TextEditingController();
+  final _taskTypeController = TextEditingController();
+  final _categoryController = TextEditingController();
 
   void clearInput() {
     _taskDescriptionController.clear;
@@ -84,76 +84,75 @@ class _NewTaskWState extends State<NewTaskW> {
                               //   builder: (runMutation, result) {
                               //     return Form(
                               //       key: _form,
-                              child: Column(
-                                children: <Widget>[
-                                  TextFormField(
-                                    autocorrect: true,
-                                    controller: _taskDescriptionController,
-                                    style:
-                                        Theme.of(context).textTheme.bodyText1,
-                                    maxLines: null,
-                                    decoration: const InputDecoration(
-                                      labelText: 'Add new task',
-                                      labelStyle: TextStyle(fontSize: 14),
-                                      border: InputBorder.none,
-                                      errorStyle:
-                                          TextStyle(color: Colors.redAccent),
-                                    ),
-                                    textInputAction: TextInputAction.next,
-                                    keyboardType: TextInputType.name,
-                                    validator: (value) {
-                                      if (value!.isEmpty) {
-                                        return 'Please provide a value.';
-                                      }
-                                      return null;
-                                    },
-                                    onFieldSubmitted: (_) {
-                                      FocusScope.of(context)
-                                          .requestFocus(_taskFocusNode);
-                                    },
-                                  ),
-                                  Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: <Widget>[
-                                      DropdownButton<String>(
-                                        hint: Text(
-                                          'Category',
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .titleSmall,
-                                        ),
-                                        icon: const Icon(Icons.arrow_downward),
-                                        iconSize: 14,
-                                        value: selectedCategory,
-                                        onChanged: (newValue) {
-                                          if (_form.currentState!.validate()) {
-                                            setState(() {
-                                              // selectedCategory = newValue;
-                                              // runMutation({
-                                              //   "task_description":
-                                              //       _taskDescriptionController.text
-                                              //           .trim(),
-                                              //   // "task_type":
-                                              //   //  _taskTypeController.text.trim(),
-                                              //   "category": _selectedCategory,
-                                              //   'userId': currUserId,
-                                              // });
-                                            });
-                                            Navigator.of(context).pop();
-                                            clearInput();
-                                          }
-                                        },
-                                        items: categories.map((category) {
-                                          return DropdownMenuItem(
-                                            child: Text(category),
-                                            value: category,
-                                          );
-                                        }).toList(),
+                              child: Form(
+                                key: _form,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    TextFormField(
+                                      autocorrect: true,
+                                      controller: _taskDescriptionController,
+                                      style:
+                                          Theme.of(context).textTheme.bodyLarge,
+                                      maxLines: null,
+                                      decoration: const InputDecoration(
+                                        labelText: 'Add new task',
+                                        labelStyle: TextStyle(fontSize: 14),
+                                        border: InputBorder.none,
+                                        errorStyle:
+                                            TextStyle(color: Colors.redAccent),
                                       ),
-                                    ],
-                                  ),
-                                ],
+                                      textInputAction: TextInputAction.next,
+                                      keyboardType: TextInputType.name,
+                                      validator: (value) {
+                                        if (value!.isEmpty) {
+                                          return 'Please provide a value.';
+                                        }
+                                        return null;
+                                      },
+                                      onFieldSubmitted: (_) {
+                                        FocusScope.of(context)
+                                            .requestFocus(_taskFocusNode);
+                                      },
+                                    ),
+                                    DropdownButton<String>(
+                                      hint: Text(
+                                        'Category',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .titleSmall,
+                                      ),
+                                      icon: const Icon(Icons.arrow_downward),
+                                      iconSize: 14,
+                                      value: selectedCategory,
+                                      onChanged: (newValue) {
+                                        if (_form.currentState!.validate()) {
+                                          setState(() {
+                                            selectedCategory = newValue;
+
+                                            // runMutation({
+                                            //   "task_description":
+                                            //       _taskDescriptionController.text
+                                            //           .trim(),
+                                            //   // "task_type":
+                                            //   //  _taskTypeController.text.trim(),
+                                            //   "category": _selectedCategory,
+                                            //   'userId': currUserId,
+                                            // });
+                                          });
+                                          Navigator.of(context).pop();
+                                          clearInput();
+                                        }
+                                      },
+                                      items: categories.map((category) {
+                                        return DropdownMenuItem(
+                                          child: Text(category),
+                                          value: category,
+                                        );
+                                      }).toList(),
+                                    ),
+                                  ],
+                                ),
                               ),
                             )
                             //],
