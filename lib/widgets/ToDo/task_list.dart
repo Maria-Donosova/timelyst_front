50cb5e0d1bd5845c8126f8b9498c638b52ee1d4f
Mariia Donosova <mariiadonosova@Mariias-MacBook-Pro.local>
2024-07-06 10:03:16 -0400

    fix reoderablelistview

--- lib/widgets/ToDo/task_list.dart
+++ lib/widgets/ToDo/task_list.dart
@@ -14,10 +14,12 @@ class TaskListW extends StatefulWidget {
 }
 
 class _TaskListWState extends State<TaskListW> {
-  //List<int> _dummyTasks = List<int>.generate(10, (int index) => index);
+  //Dummy tasks - remove once connected to database
+  // final List<String> _dummyTasks =
+  //List.generate(10, (index) => 'TaskItem $index');
   List _dummyTasks = [
     Task(
-      id: '1',
+      id: '1234',
       title: 'Try Me',
       category: 'Personal',
       dateCreated: DateTime.now(),
@@ -25,7 +27,7 @@ class _TaskListWState extends State<TaskListW> {
       creator: 'Maria Donosova',
     ),
     Task(
-      id: '2',
+      id: '21234',
       title: 'Dare',
       category: 'Work',
       dateCreated: DateTime.now(),
@@ -33,7 +35,7 @@ class _TaskListWState extends State<TaskListW> {
       creator: 'Maria Donosova',
     ),
     Task(
-      id: '3',
+      id: '35467',
       title: 'Destiny',
       category: 'Friends',
       dateCreated: DateTime.now(),
@@ -41,7 +43,7 @@ class _TaskListWState extends State<TaskListW> {
       creator: 'Maria Donosova',
     ),
     Task(
-      id: '4',
+      id: '4567',
       title: 'Work it out',
       category: 'Other',
       dateCreated: DateTime.now(),
@@ -49,7 +51,7 @@ class _TaskListWState extends State<TaskListW> {
       creator: 'Maria Donosova',
     ),
     Task(
-      id: '5',
+      id: '5124',
       title: 'Just do it',
       category: 'Family',
       dateCreated: DateTime.now(),
@@ -58,6 +60,17 @@ class _TaskListWState extends State<TaskListW> {
     ),
   ];
 
+  //Reodering for reoderable list logic
+  void _onReorder(int oldIndex, int newIndex) {
+    if (oldIndex < newIndex) {
+      newIndex -= 1;
+    }
+    final item = _dummyTasks.removeAt(oldIndex);
+    _dummyTasks.insert(newIndex, item);
+    setState(() {});
+  }
+
+  // sorting
   var _order = 'asc';
 
   List get _orderedTasks {
@@ -79,143 +92,129 @@ class _TaskListWState extends State<TaskListW> {
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (ctx, constraints) {
-        return Column(
-            //mainAxisAlignment: MainAxisAlignment.start,
-            //mainAxisSize: MainAxisSize.min,
-            children: [
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton.icon(
-                  onPressed: _changeOrder,
-                  icon: Icon(
-                    _order == 'asc' ? Icons.arrow_downward : Icons.arrow_upward,
-                  ),
-                  label: Text(
-                      'Sort ${_order == 'asc' ? 'Descending' : 'Ascending'}'),
-                ),
+        return Column(children: [
+          Align(
+            alignment: Alignment.centerRight,
+            child: TextButton.icon(
+              onPressed: _changeOrder,
+              icon: Icon(
+                _order == 'asc' ? Icons.arrow_downward : Icons.arrow_upward,
               ),
-              Expanded(
-                flex: 1,
-                child: ReorderableListView.builder(
-                    buildDefaultDragHandles: false,
-                    scrollDirection: Axis.vertical,
-                    // shrinkWrap: true,
-                    // physics: const AlwaysScrollableScrollPhysics(),
-                    onReorder: (int oldIndex, int newIndex) {
-                      setState(() {
-                        if (oldIndex < newIndex) {
-                          newIndex -= 1;
-                        }
-                        final int item = _dummyTasks.removeAt(oldIndex);
-                        _dummyTasks.insert(newIndex, item);
-                      });
-                    },
-                    itemCount: _dummyTasks.length,
-                    itemBuilder: (ctx, index) {
-                      return ReorderableDragStartListener(
-                        index: index,
-
-                        //key: Key('$index'),
-                        key: ValueKey(_dummyTasks[index]),
-                        child: Dismissible(
-                          child: TaskItem(
-                            _dummyTasks[index].id,
-                            _dummyTasks[index].title,
-                            _dummyTasks[index].category,
-                            key: UniqueKey(),
-
-                            //key: ValueKey("${task["id"]}"),
-                            //"${task["id"]}",
-                            // "${task["task_description"]}",
-                            // "${task["category"]}",
-                            // _deleteTask,
-                            // "${task['user']["id"]}",
-                          ),
-                          key: ValueKey(_dummyTasks[index]),
-                          direction: DismissDirection.horizontal,
-                          background: Container(
-                            color: Colors.orangeAccent,
-                            child: Padding(
-                              padding: const EdgeInsets.all(5),
-                              child: Row(
-                                children: const [
-                                  Text(
-                                    'Done',
-                                    style: TextStyle(color: Colors.black),
-                                  ),
-                                ],
+              label:
+                  Text('Sort ${_order == 'asc' ? 'Descending' : 'Ascending'}'),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: ReorderableListView.builder(
+              buildDefaultDragHandles: false,
+              scrollDirection: Axis.vertical,
+              itemCount: _dummyTasks.length,
+              itemBuilder: (ctx, index) {
+                final item = _dummyTasks[index];
+                return Dismissible(
+                  key: Key(item.id),
+                  direction: DismissDirection.horizontal,
+                  child: ReorderableDragStartListener(
+                    index: index,
+                    child: TaskItem(
+                      _dummyTasks[index].id,
+                      _dummyTasks[index].title,
+                      _dummyTasks[index].category,
+                      key: UniqueKey(),
+                      //key: ValueKey("${task["id"]}"),
+                      //"${task["id"]}",
+                      // "${task["task_description"]}",
+                      // "${task["category"]}",
+                      // _deleteTask,
+                      // "${task['user']["id"]}",
+                    ),
+                  ),
+                  onDismissed: (DismissDirection direction) {
+                    if (direction == DismissDirection.startToEnd) {
+                      print('Marked Completed');
+                      _dummyTasks.removeAt(index);
+                      // doneTask(
+                      //   _tasks.toString(),
+                      //   //"${task["id"]}",
+                      // );
+                    } else {
+                      print('Removed item');
+                      _dummyTasks.removeAt(index);
+                    }
+                    //deleteTask(tasks.toString()
+                    //"${task["id"]}",;
+                    //);
+                    // setState(() {
+                    //   _tasks.removeAt(index);
+                    // });
+                  },
+                  confirmDismiss: (DismissDirection direction) async {
+                    if (direction == DismissDirection.startToEnd) {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: const Text('Well Done!'),
+                      ));
+                      return true;
+                    } else {
+                      return await showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text("Delete Confirmation"),
+                            content: const Text(
+                                "Are you sure you want to delete this item?"),
+                            actions: <Widget>[
+                              TextButton(
+                                  onPressed: () async {
+                                    Navigator.of(context).pop(true);
+                                  },
+                                  child: const Text("Delete")),
+                              TextButton(
+                                onPressed: () =>
+                                    Navigator.of(context).pop(false),
+                                child: const Text("Cancel"),
                               ),
-                            ),
-                          ),
-                          secondaryBackground: Container(
-                            color: Colors.redAccent,
-                            child: Padding(
-                              padding: const EdgeInsets.all(5),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: const [
-                                  Text(
-                                    'Delete',
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          onDismissed: (DismissDirection direction) {
-                            if (direction == DismissDirection.startToEnd) {
-                              print('Marked Completed');
-                              // doneTask(
-                              //   _tasks.toString(),
-                              //   //"${task["id"]}",
-                              // );
-                            } else {
-                              print('Removed item');
-                            }
-                            //deleteTask(tasks.toString()
-                            //"${task["id"]}",;
-                            //);
-                            // setState(() {
-                            //   _tasks.removeAt(index);
-                            // });
-                          },
-                          confirmDismiss: (DismissDirection direction) async {
-                            if (direction == DismissDirection.startToEnd) {
-                              ScaffoldMessenger.of(context)
-                                  .showSnackBar(SnackBar(
-                                content: const Text('Well Done!'),
-                              ));
-                              return true;
-                            } else {
-                              return await showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return AlertDialog(
-                                    title: const Text("Delete Confirmation"),
-                                    content: const Text(
-                                        "Are you sure you want to delete this item?"),
-                                    actions: <Widget>[
-                                      TextButton(
-                                          onPressed: () async {
-                                            Navigator.of(context).pop(true);
-                                          },
-                                          child: const Text("Delete")),
-                                      TextButton(
-                                        onPressed: () =>
-                                            Navigator.of(context).pop(false),
-                                        child: const Text("Cancel"),
-                                      ),
-                                    ],
-                                  );
-                                },
-                              );
-                            }
-                          },
-                        ),
+                            ],
+                          );
+                        },
                       );
-                    }),
-              )
-            ]);
+                    }
+                  },
+                  background: Container(
+                    color: Colors.orangeAccent,
+                    child: Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Row(
+                        children: const [
+                          Text(
+                            'Done',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  secondaryBackground: Container(
+                    color: Colors.redAccent,
+                    child: Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: const [
+                          Text(
+                            'Delete',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+              onReorder: _onReorder,
+            ),
+          )
+        ]);
       },
     );
   }
