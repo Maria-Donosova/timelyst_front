a06e61d4c429efe073798fa6779da1847452b83a
Mariia Donosova <mariadonosova@gmail.com>
2024-11-28 13:50:21 -0500

    add provider package, create auth_provider file, update password validation for login to match the signup validations

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:timelyst_flutter/screens/common/connect_calendars.dart';
+
+import 'package:provider/provider.dart';
+import 'providers/auth_provider.dart';
+
 import 'package:timelyst_flutter/screens/common/log_in.dart';
 import 'package:timelyst_flutter/screens/common/sign_up.dart';
+import 'package:timelyst_flutter/screens/common/connect_calendars.dart';
 
 import 'themes.dart';
 
@@ -19,12 +23,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Sign Up',
-      theme: CustomTheme.lightTheme,
-      themeMode: currentTheme.currentTheme,
-      home: const LogInScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AuthProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Sign Up',
+        theme: CustomTheme.lightTheme,
+        themeMode: currentTheme.currentTheme,
+        home: const LogInScreen(),
+      ),
     );
   }
 }
