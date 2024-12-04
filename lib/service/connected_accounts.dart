122152d2aabe58d26265dc8fb27a5274cc8b1bb1
Mariia Donosova <mariadonosova@gmail.com>
2024-12-03 21:38:05 -0500

    add dotenv to manage varibles, update paths to refactored files

--- lib/service/connected_accounts.dart
+++ lib/service/connected_accounts.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'google_connect.dart';
+import '../data/google_connect.dart';
 
 class ConnectedAccounts with ChangeNotifier {
   final GoogleService _googleService = GoogleService();
