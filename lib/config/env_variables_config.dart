6cd97a1485614ea485a064b56010545bb002a2fe
Mariia Donosova <mariadonosova@gmail.com>
2024-12-04 16:22:10 -0500

    replace hardcoded values for backend url, google id and google secret to env variables

--- lib/config/env_variables_config.dart
+++ lib/config/env_variables_config.dart
@@ -1,9 +1,11 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Config {
-  static String get clientId => dotenv.env['CLIENT_ID'] ?? 'default_client_id';
+  static String get clientId =>
+      dotenv.env['CLIENT_ID'] ??
+      '287872468745-3s590is0k581repee2ngshs1ngucghgm.apps.googleusercontent.com';
   static String get clientSecret =>
-      dotenv.env['CLIENT_SECRET'] ?? 'default_client_secret';
+      dotenv.env['CLIENT_SECRET'] ?? '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';
   static String get backendUrl =>
       dotenv.env['BACKEND_URL'] ?? 'http://localhost:3000/auth/google/callback';
 }
