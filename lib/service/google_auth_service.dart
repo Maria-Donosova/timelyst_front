97bab3f0550d20e43658fad732255752f3bfddb1
Mariia Donosova <mariadonosova@gmail.com>
2024-12-11 10:58:10 -0500

    send authorization code and user_is back to the backend for google callback, additional logging messages

--- lib/service/google_auth_service.dart
+++ lib/service/google_auth_service.dart
@@ -1,13 +1,10 @@
 import 'dart:convert';
 
-import 'package:flutter/foundation.dart';
-
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
 import 'package:google_sign_in_web/web_only.dart';
 
+import '../service/auth_service.dart';
 import '../config/env_variables_config.dart';
-
-import 'package:http/http.dart' as http;
 
 class GoogleAuthService {
   // Method for requesting server authentication code
@@ -19,58 +16,99 @@ class GoogleAuthService {
   Future<Map<String, dynamic>> sendAuthCodeToBackend(String authCode) async {
     print('Entering sendAuthCodeToBackend Future');
     print("Auth Code: $authCode");
-
-    final response = await http.post(
-      Uri.parse(Config.backendGoogleCallback),
-      body: {
-        'code': authCode,
-      },
-    );
-
-    // Parse the response body as JSON
-    Map<String, dynamic> responseData;
     try {
-      responseData = json.decode(response.body);
-    } catch (e) {
-      print('Error parsing response JSON: $e');
-      return {
-        'success': false,
-        'message': 'Failed to parse backend response',
-      };
-    }
+      // Retrieve the JWT token from secure storage
+      final authService = AuthService();
+      final token = await authService.getAuthToken();
+      print("Token: $token");
 
-    // Check the status code and handle the response
-    if (response.statusCode == 200) {
-      print('Auth code sent to backend successfully');
-      return responseData; // Return the parsed response data
-    } else {
-      print('Failed to send Auth code to backend: ${response.statusCode}');
+      if (token == null) {
+        throw Exception('No JWT token found. Please log in again.');
+      }
+
+      // Send the HTTP POST request with the Authorization header
+      final response = await http.post(
+        Uri.parse(Config.backendGoogleCallback),
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Bearer $token', // Include the JWT token
+        },
+        body: {
+          'code': authCode,
+        },
+      );
+
+      // Check the response status code
+      if (response.statusCode == 200) {
+        print('Auth code sent to backend successfully');
+        final responseData = jsonDecode(response.body);
+        print('Response data: $responseData');
+
+        // Return the response data as a JSON object
+        return {
+          'success': true,
+          'message': 'Auth code sent to backend successfully',
+          'data': responseData,
+        };
+      } else {
+        print('Failed to send Auth code to backend: ${response.statusCode}');
+        final errorData = jsonDecode(response.body);
+        print('Error data: $errorData');
+
+        // Return the error data as a JSON object
+        return {
+          'success': false,
+          'message':
+              'Failed to send Auth code to backend: ${response.statusCode}',
+          'error': errorData,
+        };
+      }
+
+      // // Parse the response body as JSON
+      // Map<String, dynamic> responseData;
+      // try {
+      //   responseData = json.decode(response.body);
+      // } catch (e) {
+      //   print('Error parsing response JSON: $e');
+      //   return {
+      //     'success': false,
+      //     'message': 'Failed to parse backend response',
+      //   };
+      // }
+
+      // // Check the response status code
+      // if (response.statusCode == 200) {
+      //   print('Auth code sent to backend successfully');
+      //   final responseData = jsonDecode(response.body);
+      //   print('Response data: $responseData');
+
+      //   // Return the response data as a JSON object
+      //   return {
+      //     'success': true,
+      //     'message': 'Auth code sent to backend successfully',
+      //     'data': responseData,
+      //   };
+      // } else {
+      //   print('Failed to send Auth code to backend: ${response.statusCode}');
+      //   final errorData = jsonDecode(response.body);
+      //   print('Error data: $errorData');
+
+      //   // Return the error data as a JSON object
+      //   return {
+      //     'success': false,
+      //     'message':
+      //         'Failed to send Auth code to backend: ${response.statusCode}',
+      //     'error': errorData,
+      //   };
+      // }
+    } catch (e) {
+      print('Error sending Auth code to backend: $e');
       return {
         'success': false,
-        'message':
-            responseData['message'] ?? 'Failed to send Auth code to backend',
+        'message': 'Failed to send Auth code to backend',
       };
     }
   }
-
-  // // methods to manage exchanges with backend
-  // Future<bool> sendAuthCodeToBackend(String authCode) async {
-  //   print('Entering sendAuthCodeToBackend Future');
-  //   print("Auth Code: $authCode");
-  //   final response = await http.post(
-  //     Uri.parse(Config.backendGoogleCallback),
-  //     body: {
-  //       'code': authCode,
-  //     },
-  //   );
-  //   if (response.statusCode == 200) {
-  //     print('Auth code sent to backend successfully');
-  //     return true;
-  //   } else {
-  //     print('Failed to send Auth code to backend: ${response.statusCode}');
-  //     return false;
-  //   }
-  // }
 
   // Future<bool> isGoogleLoggedIn() async {
   //   try {
