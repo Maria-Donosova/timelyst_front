6ffa2b9cbf444b6d34066289c4ebf79028eb6b5a
Mariia Donosova <mariadonosova@gmail.com>
2024-12-22 14:59:58 -0500

    update references

--- lib/config/env_variables_config.dart
+++ lib/config/env_variables_config.dart
@@ -1,23 +1,19 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Config {
-  static String get clientId =>
-      dotenv.env['CLIENT_ID'] ??
-      '287872468745-3s590is0k581repee2ngshs1ngucghgm.apps.googleusercontent.com';
-  static String get clientSecret =>
-      dotenv.env['CLIENT_SECRET'] ?? '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';
-  static String get googleOath =>
-      dotenv.env['GOOGLE_OATH_URL'] ?? 'https://oauth2.googleapis.com/token';
-  static String get googleOath2Token =>
-      dotenv.env['GOOGLE_OATH2_TOKEN_URL'] ??
-      'https://accounts.google.com/o/oauth2/token';
+  // Google OAuth2
+  static String? get clientId => dotenv.env['CLIENT_ID']!;
+  static String? get clientSecret => dotenv.env['CLIENT_SECRET']!;
+  static String? get googleOath => dotenv.env['GOOGLE_OATH_URL']!;
+  static String? get googleOath2Token => dotenv.env['GOOGLE_OATH2_TOKEN_URL']!;
 
+// Backend google APIs
   static String get backendGoogleCallback =>
-      dotenv.env['BACKEND_GOOGLE_CALLBACK'] ??
-      'http://localhost:3000/auth/google/callback';
-  static String get redirectUri =>
-      dotenv.env['REDIRECT_URI'] ??
-      'http://localhost:3000/auth/google/callback';
+      dotenv.env['BACKEND_GOOGLE_CALLBACK']!;
+  static String get backendGoogleCalendars =>
+      dotenv.env['BACKEND_GOOGLE_CALENDARS']!;
+  //static String? get redirectUri => dotenv.env['REDIRECT_URI'];
+
   static String get frontendURL =>
       dotenv.env['FRONTEND_URL'] ?? 'http://localhost:7357';
 }
