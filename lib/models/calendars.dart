be8986b7d9784ae7dae3973040b230e5741e3754
Mariia Donosova <mariadonosova@gmail.com>
2024-12-25 21:00:25 -0500

    update calendar model to reflect mandatory vs optional fields

--- lib/models/calendars.dart
+++ lib/models/calendars.dart
@@ -1,22 +1,24 @@
 import 'dart:ui';
 
 class Calendar {
+  final String kind;
   final String etag;
   final String id;
   final String title;
-  final String description;
+  final String? description;
   final String timeZone;
-  final String category;
-  final Color catColor;
-  final String defaultReminders;
-  final String notificationSettings;
-  final String conferenceProperties;
+  final String? category;
+  final Color? catColor;
+  final String? defaultReminders;
+  final String? notificationSettings;
+  final String? conferenceProperties;
 
   Calendar({
+    required this.kind,
     required this.etag,
     required this.id,
     required this.title,
-    required this.description,
+    this.description,
     this.timeZone = '',
     this.category = '',
     this.catColor = const Color(0xFF000000),
@@ -28,22 +30,24 @@ class Calendar {
   // Convert JSON to Calendar object
   factory Calendar.fromJson(Map<String, dynamic> json) {
     return Calendar(
-      etag: json['etag'],
-      id: json['id'],
-      title: json['summary'],
-      description: json['description'],
-      timeZone: json['timeZone'],
-      category: json['category'],
-      catColor: json['catColor'],
-      defaultReminders: json['defaultReminders'],
-      notificationSettings: json['notificationSettings'],
-      conferenceProperties: json['conferenceProperties'],
+      kind: json['kind'] ?? '',
+      etag: json['etag'] ?? '',
+      id: json['id'] ?? '',
+      title: json['summary'] ?? '',
+      description: json['description'] ?? '',
+      timeZone: json['timeZone'] ?? '',
+      category: json['category'] ?? '',
+      catColor: json['catColor'] ?? '',
+      defaultReminders: json['defaultReminders'] ?? '',
+      notificationSettings: json['notificationSettings'] ?? '',
+      conferenceProperties: json['conferenceProperties'] ?? '',
     );
   }
 
   // Convert Calendar object to JSON
   Map<String, dynamic> toJson() {
     return {
+      'kind': kind,
       'etag': etag,
       'id': id,
       'summary': title,
