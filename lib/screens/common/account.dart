7f32863d1262fd2dd4d1fda8c875f826e6e86977
Mariia Donosova <mariadonosova@gmail.com>
2025-02-06 20:07:20 -0500

    update appBar routing to route to accountSettings, layout changes for Account Settings

--- lib/screens/common/account.dart
+++ lib/screens/common/account.dart
@@ -10,27 +10,76 @@ class Account extends StatelessWidget {
   Widget build(BuildContext context) {
     final appBar = CustomAppBar();
 
+    Widget _buildSectionHeader(String title) {
+      return Container(
+        color: Theme.of(context).colorScheme.secondary,
+        padding: const EdgeInsets.all(8),
+        margin: const EdgeInsets.only(bottom: 10),
+        child: Text(
+          title,
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onSecondary,
+            fontSize: Theme.of(context).textTheme.bodyMedium?.fontSize,
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: appBar,
       body: SafeArea(
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text("Account User Name"),
-              Text("Account User Last Name"),
-              Text("Email"),
-              Text("Password"),
-            ],
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 50.0),
+            child: Text(
+              "Account Settings",
+              style: Theme.of(context).textTheme.titleMedium,
+              textAlign: TextAlign.center,
+            ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text("Connected accounts"),
-              Text("Email"),
-              Text("Calendards"),
-            ],
-          )
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildSectionHeader("Connected accounts & calendars"),
+                Row(
+                  children: [
+                    Text("Email"),
+                    Text("Calendards"),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildSectionHeader("User Settings"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      //crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text("First Name"),
+                        Text("Last Name"),
+                      ],
+                    ),
+                    Column(
+                      //crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text("Email"),
+                        Text("Password"),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ]),
       ),
     );
