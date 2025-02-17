207e65da275191553a0b192cf0931a4ee38f1997
Mariia Donosova <mariadonosova@gmail.com>
2025-02-16 19:30:39 -0500

    aupdate customAppBar so it fetches userId from authService getUserIdFromToken method

--- lib/widgets/shared/customAppbar.dart
+++ lib/widgets/shared/customAppbar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../services/authService.dart';
-
 import '../../screens/common/connectCalendars.dart';
 import '../../screens/common/account.dart';
 import '../../screens/common/signUp.dart';
@@ -13,9 +12,7 @@ enum _timelystMenu { about, contact_us }
 enum _profileView { profile, settings, logout }
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  CustomAppBar({
-    Key? key,
-  }) : super(key: key);
+  CustomAppBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -36,9 +33,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   Size get preferredSize => Size.fromHeight(kToolbarHeight);
 
   Widget _buildTitle() {
-    return Text(
-      'Tame the Time',
-    );
+    return Text('Tame the Time');
   }
 
   Widget _buildLeading(BuildContext context) {
@@ -63,46 +58,48 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   List<Widget> _buildActions(BuildContext context, AuthProvider authProvider) {
     final AuthService authService = AuthService();
 
-    // Use the authProvider to determine the actions
     return [
       if (authProvider.isLoggedIn) ...[
         PopupMenuButton(
           tooltip: 'Account',
-          icon: Icon(
-            Icons.menu_outlined,
-          ),
+          icon: Icon(Icons.menu_outlined),
           elevation: 8,
           itemBuilder: (BuildContext context) => <PopupMenuEntry<_profileView>>[
             PopupMenuItem<_profileView>(
               value: _profileView.profile,
               child: Text('Account Settings'),
               onTap: () async {
-                print('Account Settings');
-                //final result;
-
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-                    builder: (context) => AccountSettings(
-                          authService: authService,
-                          userId: '6796c7cae4edbe922aaded37',
-                        )));
+                final userId = await authService.getUserIdFromToken();
+                if (userId != null) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AccountSettings(
+                        authService: authService,
+                        userId: userId,
+                      ),
+                    ),
+                  );
+                }
               },
             ),
             PopupMenuItem<_profileView>(
               value: _profileView.settings,
               child: Text('Connect Calendars'),
               onTap: () {
-                print('Connect Calendars');
-                Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => ConnectCal()));
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ConnectCal()),
+                );
               },
             ),
             PopupMenuItem<_profileView>(
               value: _profileView.logout,
               child: Text('Logout'),
               onTap: () {
-                print('Logging out');
                 authProvider.logout();
-                Navigator.of(context).pushReplacement(
+                Navigator.pushReplacement(
+                  context,
                   MaterialPageRoute(builder: (context) => LogInScreen()),
                 );
               },
@@ -116,16 +113,148 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
                 color: Theme.of(context).colorScheme.onSecondary,
               )),
           onPressed: () {
-            //_saveForm,
-            print('sign up button pressed');
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => const SignUpScreen()),
             );
-            // }
           },
         ),
       ]
     ];
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import '../../services/authService.dart';
+
+// import '../../screens/common/connectCalendars.dart';
+// import '../../screens/common/account.dart';
+// import '../../screens/common/signUp.dart';
+// import '../../screens/common/logIn.dart';
+// import '../../providers/authProvider.dart';
+
+// enum _timelystMenu { about, contact_us }
+
+// enum _profileView { profile, settings, logout }
+
+// class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+//   CustomAppBar({
+//     Key? key,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final authProvider = Provider.of<AuthProvider>(context);
+
+//     return AppBar(
+//       title: _buildTitle(),
+//       centerTitle: true,
+//       backgroundColor: Colors.white,
+//       elevation: 3,
+//       shadowColor: Theme.of(context).shadowColor,
+//       leading: _buildLeading(context),
+//       actions: _buildActions(context, authProvider),
+//     );
+//   }
+
+//   @override
+//   Size get preferredSize => Size.fromHeight(kToolbarHeight);
+
+//   Widget _buildTitle() {
+//     return Text(
+//       'Tame the Time',
+//     );
+//   }
+
+//   Widget _buildLeading(BuildContext context) {
+//     return PopupMenuButton(
+//       tooltip: 'About',
+//       icon: Image.asset("assets/images/logos/timelyst_logo.png"),
+//       iconSize: 20,
+//       elevation: 8,
+//       itemBuilder: (BuildContext context) => <PopupMenuEntry<_timelystMenu>>[
+//         const PopupMenuItem<_timelystMenu>(
+//           value: _timelystMenu.about,
+//           child: Text('About Us'),
+//         ),
+//         const PopupMenuItem<_timelystMenu>(
+//           value: _timelystMenu.contact_us,
+//           child: Text('Contact'),
+//         ),
+//       ],
+//     );
+//   }
+
+//   List<Widget> _buildActions(BuildContext context, AuthProvider authProvider) {
+//     print("Entering _buildActions in customAppBar");
+//     final AuthService authService = AuthService();
+//     final userId = authService.getUserIdFromToken();
+//     print("UserId: $userId");
+
+//     // Use the authProvider to determine the actions
+//     return [
+//       if (authProvider.isLoggedIn) ...[
+//         PopupMenuButton(
+//           tooltip: 'Account',
+//           icon: Icon(
+//             Icons.menu_outlined,
+//           ),
+//           elevation: 8,
+//           itemBuilder: (BuildContext context) => <PopupMenuEntry<_profileView>>[
+//             PopupMenuItem<_profileView>(
+//               value: _profileView.profile,
+//               child: Text('Account Settings'),
+//               onTap: () async {
+//                 print('Account Settings');
+//                 //final result;
+
+//                 Navigator.of(context).pushReplacement(MaterialPageRoute(
+//                     builder: (context) => AccountSettings(
+//                           authService: authService,
+//                           userId: userId,
+//                         )));
+//               },
+//             ),
+//             PopupMenuItem<_profileView>(
+//               value: _profileView.settings,
+//               child: Text('Connect Calendars'),
+//               onTap: () {
+//                 print('Connect Calendars');
+//                 Navigator.of(context).pushReplacement(
+//                     MaterialPageRoute(builder: (context) => ConnectCal()));
+//               },
+//             ),
+//             PopupMenuItem<_profileView>(
+//               value: _profileView.logout,
+//               child: Text('Logout'),
+//               onTap: () {
+//                 print('Logging out');
+//                 authProvider.logout();
+//                 Navigator.of(context).pushReplacement(
+//                   MaterialPageRoute(builder: (context) => LogInScreen()),
+//                 );
+//               },
+//             ),
+//           ],
+//         ),
+//       ] else ...[
+//         TextButton(
+//           child: Text('Sign Up',
+//               style: TextStyle(
+//                 color: Theme.of(context).colorScheme.onSecondary,
+//               )),
+//           onPressed: () {
+//             //_saveForm,
+//             print('sign up button pressed');
+//             Navigator.push(
+//               context,
+//               MaterialPageRoute(builder: (context) => const SignUpScreen()),
+//             );
+//             // }
+//           },
+//         ),
+//       ]
+//     ];
+//   }
+// }
