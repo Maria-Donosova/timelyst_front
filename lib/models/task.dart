ab762e297368d13a9f6a9e5f64ed5e87cd40fd57
Mariia Donosova <mariadonosova@gmail.com>
2025-02-25 20:24:16 -0500

    add json serialization within tasks model

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -3,7 +3,6 @@ class Task {
   final String title;
   final String task_type;
   final String category;
-  //final Color category;
   final DateTime dateCreated;
   final DateTime dateChanged;
   final String creator;
@@ -13,9 +12,56 @@ class Task {
     required this.title,
     this.task_type = "Task",
     this.category = 'Work',
-    //this.category = Colors.grey,
     required this.dateCreated,
     required this.dateChanged,
     required this.creator,
   });
+
+  // Convert JSON to Task object
+  factory Task.fromJson(Map<String, dynamic> json) {
+    return Task(
+      id: json['id'],
+      title: json['title'],
+      task_type: json['task_type'],
+      category: json['category'],
+      dateCreated: DateTime.parse(json['dateCreated']),
+      dateChanged: DateTime.parse(json['dateChanged']),
+      creator: json['creator'],
+    );
+  }
+
+  // Convert Task object to JSON
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'task_type': task_type,
+      'category': category,
+      'dateCreated': dateCreated.toIso8601String(),
+      'dateChanged': dateChanged.toIso8601String(),
+      'creator': creator,
+    };
+  }
 }
+
+// class Task {
+//   final String id;
+//   final String title;
+//   final String task_type;
+//   final String category;
+//   //final Color category;
+//   final DateTime dateCreated;
+//   final DateTime dateChanged;
+//   final String creator;
+
+//   Task({
+//     required this.id,
+//     required this.title,
+//     this.task_type = "Task",
+//     this.category = 'Work',
+//     //this.category = Colors.grey,
+//     required this.dateCreated,
+//     required this.dateChanged,
+//     required this.creator,
+//   });
+// }
