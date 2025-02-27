05dbf79ecd3ff38abf39856fce7b6c875b884f2b
Mariia Donosova <mariadonosova@gmail.com>
2025-02-26 21:06:27 -0500

    amend to full statful widget (boiler plate), and inject TasksService delete future

--- lib/widgets/ToDo/delete_task.dart
+++ lib/widgets/ToDo/delete_task.dart
@@ -1,15 +1,56 @@
-List tasks = ['task', 'task1', 'task2'];
+import 'package:flutter/material.dart';
+import 'package:timelyst_flutter/data/tasks.dart';
+import '../../models/task.dart';
 
-void deleteTask(String id) {
-  tasks.removeWhere((tasks) => tasks.id == id);
+class DeleteTaskW extends StatefulWidget {
+  final Task task;
+
+  DeleteTaskW({required this.task, super.key});
+
+  @override
+  State<DeleteTaskW> createState() => _DeleteTaskWState();
 }
 
-String removeTask() {
-  return """
-  mutation removeTask(\$id: String!){
-    removeTask(id: \$id) {
-      task_description
+class _DeleteTaskWState extends State<DeleteTaskW> {
+  final List tasks = [
+    'task',
+    'task1',
+    'task2'
+  ]; //replace with the list that gets fetched from the backend
+  void deleteTask(String taskId) async {
+    try {
+      await TasksService.deleteTask(
+          taskId, 'authToken'); // Replace with actual auth token
+      // Remove the task from the local list
+      setState(() {
+        tasks.removeWhere((task) => task.id == taskId);
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to delete task: $e')),
+      );
     }
   }
-  """;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
 }
+
+// List tasks = ['task', 'task1', 'task2'];
+
+// void deleteTask(String id) {
+//   tasks.removeWhere((tasks) => tasks.id == id);
+// }
+
+// String removeTask() {
+//   return """
+//   mutation removeTask(\$id: String!){
+//     removeTask(id: \$id) {
+//       task_description
+//     }
+//   }
+//   """;
+// }
