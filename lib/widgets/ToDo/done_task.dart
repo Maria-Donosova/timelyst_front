f8f67c5d9b3fa8a45b0dd85db9a2eec69fcdc34b
Mariia Donosova <mariadonosova@gmail.com>
2025-02-27 22:07:51 -0500

    remove unused code, re-org comments

--- lib/widgets/ToDo/done_task.dart
+++ lib/widgets/ToDo/done_task.dart
@@ -40,20 +40,3 @@ class _DoneTaskWState extends State<DoneTaskW> {
     throw UnimplementedError();
   }
 }
-
-// void doneTask(String id) {
-//   tasks.removeWhere((tasks) => tasks.id == id);
-//   tasks.where((tasks) => tasks.id == id);
-//   isDone = true;
-// }
-
-//backend: schema & method should be updated & developed
-// String markCompleteTask() {
-//   return """
-//   mutation markCompelte(\$id: String!){
-//     markCompelte(id: \$id) {
-//       status
-//     }
-//   }
-//   """;
-// }
