0eb268eaa65434b262aa5abca0f3d82de50c699d
Mariia Donosova <mariadonosova@gmail.com>
2025-04-19 16:36:20 -0400

    adjust calendars to meet backend types and resolvers

--- lib/data/calendars.dart
+++ lib/data/calendars.dart
@@ -10,16 +10,37 @@ class CalendarsService {
 
     // Define the GraphQL query string
     final String query = '''
-        query User(\$userId: String!) {
-          user(id: \$userId) {
-            id
-            email
-            googleAccounts {
-              email
-              selectedCalendars {
-                id
-                summary
+        query {
+          calendars {
+            calendars {
+              id
+              user
+              sourceCalendar
+              calendarId
+              etag
+              kind
+              summary
+              ownerAccount
+              description
+              timeZone
+              defaultReminders {
+                method
+                minutes
               }
+              notificationSettings {
+                notifications {
+                  type
+                  method
+                }
+              }
+              calendarPrimary
+              category
+              catColor
+              conferenceProperties {
+                allowedConferenceSolutionTypes
+              }
+              createdAt
+              updatedAt
             }
           }
         }
@@ -34,7 +55,6 @@ class CalendarsService {
       },
       body: jsonEncode({
         'query': query,
-        'variables': {'userId': userId},
       }),
     );
 
@@ -51,15 +71,12 @@ class CalendarsService {
             'Fetching calendars failed: ${errors.map((e) => e['message']).join(", ")}');
       }
 
-      // Extract the user data from the response
-      final userData = data['data']['user'];
+      // Extract the calendars from the response
+      final calendarsData = data['data']['calendars']['calendars'];
 
       // Parse the calendars into a List<Calendar>
-      final List<Calendar> calendars = (userData['googleAccounts'] as List?)
-              ?.expand((googleAccount) =>
-                  (googleAccount['selectedCalendars'] as List?)
-                      ?.map((calendar) => Calendar.fromJson(calendar)) ??
-                  <Calendar>[])
+      final List<Calendar> calendars = (calendarsData as List?)
+              ?.map((calendar) => Calendar.fromJson(calendar))
               .toList() ??
           [];
 
@@ -74,37 +91,190 @@ class CalendarsService {
     }
   }
 
-  static Future<void> updateCalendar(
-      String calendarId, String authToken, Calendar updatedCalendar) async {
-    print("Entering updateCalendar in CalendarsService");
+  static Future<Calendar> fetchCalendar(
+      String calendarId, String authToken) async {
+    print("Entering fetchCalendar in CalendarsService");
+
+    // Define the GraphQL query string
+    final String query = '''
+        query Calendar(\$id: String!) {
+          calendar(id: \$id) {
+            id
+            user
+            sourceCalendar
+            calendarId
+            etag
+            kind
+            summary
+            ownerAccount
+            description
+            timeZone
+            defaultReminders {
+              method
+              minutes
+            }
+            notificationSettings {
+              notifications {
+                type
+                method
+              }
+            }
+            calendarPrimary
+            category
+            catColor
+            conferenceProperties {
+              allowedConferenceSolutionTypes
+            }
+            createdAt
+            updatedAt
+          }
+        }
+    ''';
+
+    final response = await http.post(
+      Uri.parse(Config.backendGraphqlURL),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $authToken',
+      },
+      body: jsonEncode({
+        'query': query,
+        'variables': {'id': calendarId},
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      if (data['errors'] != null && data['errors'].length > 0) {
+        final errors = data['errors'];
+        print('Fetching calendar failed with errors: $errors');
+        throw Exception(
+            'Fetching calendar failed: ${errors.map((e) => e['message']).join(", ")}');
+      }
+
+      final calendarData = data['data']['calendar'];
+      return Calendar.fromJson(calendarData);
+    } else {
+      print('Failed to fetch calendar: ${response.statusCode}');
+      throw Exception('Failed to fetch calendar: ${response.statusCode}');
+    }
+  }
+
+  static Future<Calendar> createCalendar(
+      String authToken, Calendar calendar) async {
+    print("Entering createCalendar in CalendarsService");
 
     // Define the GraphQL mutation string
     final String mutation = '''
-        mutation UpdateCalendar(\$calendarId: String!, \$input: CalendarInput!) {
-          updateCalendar(id: \$calendarId, input: \$input) {
+        mutation CreateCalendar(\$input: CalendarInput!) {
+          createCalendar(input: \$input) {
             id
+            user
+            sourceCalendar
+            calendarId
+            etag
+            kind
             summary
+            ownerAccount
             description
             timeZone
+            defaultReminders {
+              method
+              minutes
+            }
+            notificationSettings {
+              notifications {
+                type
+                method
+              }
+            }
+            calendarPrimary
             category
             catColor
-            defaultReminders
-            notificationSettings
-            conferenceProperties
-            organizer
-            recipients
-            importAll
-            importSubject
-            importBody
-            importConferenceInfo
-            importOrganizer
-            importRecipients
+            conferenceProperties {
+              allowedConferenceSolutionTypes
+            }
+            createdAt
+            updatedAt
           }
         }
     ''';
 
     final Map<String, dynamic> variables = {
-      'calendarId': calendarId,
+      'input': calendar.toJson(email: calendar.user),
+    };
+
+    final response = await http.post(
+      Uri.parse(Config.backendGraphqlURL),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $authToken',
+      },
+      body: jsonEncode({
+        'query': mutation,
+        'variables': variables,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      if (data['errors'] != null && data['errors'].length > 0) {
+        final errors = data['errors'];
+        print('Creating calendar failed with errors: $errors');
+        throw Exception(
+            'Creating calendar failed: ${errors.map((e) => e['message']).join(", ")}');
+      }
+
+      final calendarData = data['data']['createCalendar'];
+      return Calendar.fromJson(calendarData);
+    } else {
+      print('Failed to create calendar: ${response.statusCode}');
+      throw Exception('Failed to create calendar: ${response.statusCode}');
+    }
+  }
+
+  static Future<Calendar> updateCalendar(
+      String calendarId, String authToken, Calendar updatedCalendar) async {
+    print("Entering updateCalendar in CalendarsService");
+
+    // Define the GraphQL mutation string
+    final String mutation = '''
+        mutation UpdateCalendar(\$id: String!, \$input: CalendarInput!) {
+          updateCalendar(id: \$id, input: \$input) {
+            id
+            user
+            sourceCalendar
+            calendarId
+            etag
+            kind
+            summary
+            ownerAccount
+            description
+            timeZone
+            defaultReminders {
+              method
+              minutes
+            }
+            notificationSettings {
+              notifications {
+                type
+                method
+              }
+            }
+            calendarPrimary
+            category
+            catColor
+            conferenceProperties {
+              allowedConferenceSolutionTypes
+            }
+            createdAt
+            updatedAt
+          }
+        }
+    ''';
+
+    final Map<String, dynamic> variables = {
+      'id': calendarId,
       'input': updatedCalendar.toJson(email: updatedCalendar.user),
     };
 
@@ -128,21 +298,23 @@ class CalendarsService {
         throw Exception(
             'Updating calendar failed: ${errors.map((e) => e['message']).join(", ")}');
       }
-      print('Calendar updated successfully');
+
+      final calendarData = data['data']['updateCalendar'];
+      return Calendar.fromJson(calendarData);
     } else {
       print('Failed to update calendar: ${response.statusCode}');
       throw Exception('Failed to update calendar: ${response.statusCode}');
     }
   }
 
-  static Future<void> deleteCalendar(
+  static Future<bool> deleteCalendar(
       String calendarId, String authToken) async {
     print("Entering deleteCalendar in CalendarsService");
 
     // Define the GraphQL mutation string
     final String mutation = '''
-        mutation DeleteCalendar(\$calendarId: String!) {
-          deleteCalendar(id: \$calendarId)
+        mutation DeleteCalendar(\$id: String!) {
+          deleteCalendar(id: \$id)
         }
     ''';
 
@@ -154,7 +326,7 @@ class CalendarsService {
       },
       body: jsonEncode({
         'query': mutation,
-        'variables': {'calendarId': calendarId},
+        'variables': {'id': calendarId},
       }),
     );
 
@@ -166,95 +338,13 @@ class CalendarsService {
         throw Exception(
             'Deleting calendar failed: ${errors.map((e) => e['message']).join(", ")}');
       }
+
+      final result = data['data']['deleteCalendar'];
       print('Calendar deleted successfully');
+      return result;
     } else {
       print('Failed to delete calendar: ${response.statusCode}');
       throw Exception('Failed to delete calendar: ${response.statusCode}');
-    }
-  }
-
-  static Future<void> updateCategory(
-      String calendarId, String authToken, String category) async {
-    print("Entering updateCategory in CalendarsService");
-
-    // Define the GraphQL mutation string
-    final String mutation = '''
-        mutation UpdateCategory(\$calendarId: String!, \$category: String!) {
-          updateCategory(id: \$calendarId, category: \$category) {
-            id
-            category
-          }
-        }
-    ''';
-
-    final response = await http.post(
-      Uri.parse(Config.backendGraphqlURL),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $authToken',
-      },
-      body: jsonEncode({
-        'query': mutation,
-        'variables': {'calendarId': calendarId, 'category': category},
-      }),
-    );
-
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      if (data['errors'] != null && data['errors'].length > 0) {
-        final errors = data['errors'];
-        print('Updating category failed with errors: $errors');
-        throw Exception(
-            'Updating category failed: ${errors.map((e) => e['message']).join(", ")}');
-      }
-      print('Category updated successfully');
-    } else {
-      print('Failed to update category: ${response.statusCode}');
-      throw Exception('Failed to update category: ${response.statusCode}');
-    }
-  }
-
-  static Future<void> updateNotifications(String calendarId, String authToken,
-      Map<String, dynamic> notificationSettings) async {
-    print("Entering updateNotifications in CalendarsService");
-
-    // Define the GraphQL mutation string
-    final String mutation = '''
-        mutation UpdateNotifications(\$calendarId: String!, \$notificationSettings: NotificationSettingsInput!) {
-          updateNotifications(id: \$calendarId, notificationSettings: \$notificationSettings) {
-            id
-            notificationSettings
-          }
-        }
-    ''';
-
-    final response = await http.post(
-      Uri.parse(Config.backendGraphqlURL),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $authToken',
-      },
-      body: jsonEncode({
-        'query': mutation,
-        'variables': {
-          'calendarId': calendarId,
-          'notificationSettings': notificationSettings,
-        },
-      }),
-    );
-
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      if (data['errors'] != null && data['errors'].length > 0) {
-        final errors = data['errors'];
-        print('Updating notifications failed with errors: $errors');
-        throw Exception(
-            'Updating notifications failed: ${errors.map((e) => e['message']).join(", ")}');
-      }
-      print('Notifications updated successfully');
-    } else {
-      print('Failed to update notifications: ${response.statusCode}');
-      throw Exception('Failed to update notifications: ${response.statusCode}');
     }
   }
 }
