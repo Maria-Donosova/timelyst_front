217eb5f62776d1dceb3112b819baef19ec1ce164
Mariia Donosova <mariadonosova@gmail.com>
2025-05-10 15:00:17 -0400

    move a select recurrence metho into a separate widget

--- lib/widgets/calendar/views/calendar_selection_widget.dart
+++ lib/widgets/calendar/views/calendar_selection_widget.dart
@@ -48,6 +48,7 @@ class _CalendarSelectionWidgetState extends State<CalendarSelectionWidget> {
 
   @override
   Widget build(BuildContext context) {
+    print("Entering calendar selection widget");
     return AlertDialog(
       title:
           Text('Calendars', style: Theme.of(context).textTheme.displayMedium),
