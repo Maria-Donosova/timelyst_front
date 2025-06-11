3b1a0a15e42d0aae3f66291a31466818088175ac
Mariia Donosova <mariadonosova@gmail.com>
2025-06-10 20:37:07 -0400

    group calendars by category and display calendars for each category

--- lib/widgets/calendar/views/calendar_selection_widget.dart
+++ lib/widgets/calendar/views/calendar_selection_widget.dart
@@ -4,125 +4,94 @@ import '../../shared/categories.dart';
 
 class CalendarSelectionWidget extends StatefulWidget {
   final List<Calendar> calendars;
-  final Map<String, bool>? initialSelection;
+  final Map<String, bool>? initialSelectedCalendars;
 
   const CalendarSelectionWidget({
     Key? key,
     required this.calendars,
-    this.initialSelection,
+    this.initialSelectedCalendars,
   }) : super(key: key);
 
   @override
-  State<CalendarSelectionWidget> createState() =>
+  _CalendarSelectionWidgetState createState() =>
       _CalendarSelectionWidgetState();
 }
 
 class _CalendarSelectionWidgetState extends State<CalendarSelectionWidget> {
-  late Map<String, bool> _checkedCalendars;
-  late Set<String> _uniqueCategories;
-  late Map<String, List<Calendar>> _categoryCalendarsMap;
+  late Map<String, bool> _selectedCalendars;
 
   @override
   void initState() {
     super.initState();
-    _checkedCalendars = widget.initialSelection ?? {};
-    _uniqueCategories = {};
-    _categoryCalendarsMap = {};
-
-    // Populate the Set and Map with unique categories and their calendars
+    _selectedCalendars =
+        Map<String, bool>.from(widget.initialSelectedCalendars ?? {});
+    // Ensure all calendars have an entry, defaulting to false if not in initialSelectedCalendars
     for (var calendar in widget.calendars) {
-      final category = calendar.category;
-
-      if (!_uniqueCategories.contains(category)) {
-        _uniqueCategories.add(category!);
-        _categoryCalendarsMap[category] = []; // Initialize with an empty list
-      }
-      _categoryCalendarsMap[category]!.add(calendar);
-
-      // Initialize checked state if not already set
-      if (!_checkedCalendars.containsKey(calendar.title)) {
-        _checkedCalendars[calendar.title] = false;
-      }
+      _selectedCalendars.putIfAbsent(calendar.id!, () => false);
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    print("Entering calendar selection widget");
+    // Group calendars by category
+    Map<String, List<Calendar>> groupedCalendars = {};
+    for (var calendar in widget.calendars) {
+      String category = calendar.category ?? 'Uncategorized';
+      if (!groupedCalendars.containsKey(category)) {
+        groupedCalendars[category] = [];
+      }
+      groupedCalendars[category]!.add(calendar);
+    }
+
     return AlertDialog(
-      title:
-          Text('Calendars', style: Theme.of(context).textTheme.displayMedium),
+      title: const Text('Select Calendar(s)'),
       content: SingleChildScrollView(
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: _uniqueCategories.map((category) {
-            final categoryCalendars = _categoryCalendarsMap[category]!;
+          children: groupedCalendars.entries.map((entry) {
+            String category = entry.key;
+            List<Calendar> calendarsInCategory = entry.value;
             return Column(
-              mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        category,
-                        style: Theme.of(context).textTheme.bodyLarge,
-                      ),
-                    ),
-                    CircleAvatar(
-                      backgroundColor: catColor(category),
-                      radius: 4.5,
-                    ),
-                  ],
-                ),
-                if (categoryCalendars.isNotEmpty)
-                  Column(
-                    children: categoryCalendars.map((calendar) {
-                      return Tooltip(
-                        message: calendar.user,
-                        child: CheckboxListTile(
-                          title: Text(
-                            calendar.title,
-                            style: Theme.of(context).textTheme.bodyMedium,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          value: _checkedCalendars[calendar.title],
-                          onChanged: (bool? value) {
-                            setState(() {
-                              _checkedCalendars[calendar.title] = value!;
-                            });
-                          },
-                        ),
-                      );
-                    }).toList(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text(
+                    category,
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium
+                        ?.copyWith(fontWeight: FontWeight.bold),
                   ),
+                ),
+                ...calendarsInCategory.map((calendar) {
+                  return CheckboxListTile(
+                    title: Text(calendar.title), // Displays calendar name
+                    value: _selectedCalendars[calendar.id!],
+                    onChanged: (bool? value) {
+                      setState(() {
+                        _selectedCalendars[calendar.id!] = value!;
+                      });
+                    },
+                  );
+                }).toList(),
+                Divider(),
               ],
             );
           }).toList(),
         ),
       ),
-      actions: [
+      actions: <Widget>[
         TextButton(
-          style: ElevatedButton.styleFrom(
-              backgroundColor: Theme.of(context).colorScheme.secondary),
-          child: Text('Cancel',
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.onPrimary,
-              )),
+          child: const Text('Cancel'),
           onPressed: () {
             Navigator.of(context).pop();
           },
         ),
         TextButton(
-          style: ElevatedButton.styleFrom(
-              backgroundColor: Theme.of(context).colorScheme.secondary),
-          child: Text('Save',
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.onPrimary,
-              )),
+          child: const Text('OK'),
           onPressed: () {
-            Navigator.of(context).pop(_checkedCalendars);
+            Navigator.of(context).pop(_selectedCalendars);
           },
         ),
       ],
@@ -133,13 +102,16 @@ class _CalendarSelectionWidgetState extends State<CalendarSelectionWidget> {
 // Helper function to show the calendar selection dialog
 Future<Map<String, bool>?> showCalendarSelectionDialog(
     BuildContext context, List<Calendar> calendars,
+    // Stylistic suggestion: consider renaming for consistency with the widget's parameter
+    // {Map<String, bool>? initialSelectedCalendars}) async {
     {Map<String, bool>? initialSelection}) async {
   return await showDialog<Map<String, bool>>(
     context: context,
     builder: (BuildContext context) {
       return CalendarSelectionWidget(
         calendars: calendars,
-        initialSelection: initialSelection,
+        // initialSelectedCalendars: initialSelectedCalendars, // if renamed above
+        initialSelectedCalendars: initialSelection,
       );
     },
   );
