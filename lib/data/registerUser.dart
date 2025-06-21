e8a9cc3c20d3f56be324fce7a090f87fb865cf43
Mariia Donosova <mariadonosova@gmail.com>
2025-06-21 19:49:23 -0400

    rename environment_variables_config back to envVarConfig and update imports

--- lib/data/registerUser.dart
+++ lib/data/registerUser.dart
@@ -1,5 +1,5 @@
 import 'package:http/http.dart' as http;
-import 'package:timelyst_flutter/config/env_variables_config.dart';
+import 'package:timelyst_flutter/config/envVarConfig.dart';
 import 'dart:convert';
 
 import '../services/authService.dart';
