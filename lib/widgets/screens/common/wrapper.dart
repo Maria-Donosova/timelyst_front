14d70c7921d052072bbecbc4f82f0fb68e2e1d54
Mariia Donosova <mariadonosova@gmail.com>
2025-09-27 19:59:53 -0400

    fix: update frontend redirect uri, update wrapper to detect oath callback

--- lib/widgets/screens/common/wrapper.dart
+++ lib/widgets/screens/common/wrapper.dart
@@ -3,11 +3,20 @@ import 'package:provider/provider.dart';
 import 'package:timelyst_flutter/providers/authProvider.dart';
 import 'package:timelyst_flutter/widgets/screens/common/agenda.dart';
 import 'package:timelyst_flutter/widgets/screens/common/logIn.dart';
+import 'package:timelyst_flutter/widgets/screens/common/microsoftOAuthCallback.dart';
+import 'dart:html' as html;
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final authProvider = Provider.of<AuthProvider>(context);
+    
+    // Check if this is a Microsoft OAuth callback
+    final uri = Uri.parse(html.window.location.href);
+    if (uri.queryParameters.containsKey('code')) {
+      print('🔍 [Wrapper] Detected Microsoft OAuth callback');
+      return MicrosoftOAuthCallback();
+    }
 
     if (authProvider.isLoggedIn) {
       return Agenda();
